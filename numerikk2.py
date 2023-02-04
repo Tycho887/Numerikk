1f4498ab49851b451ac21bd70f2c8ca7cf76bc2d
Tycho887 <michaelrj03@gmail.com>
2023-02-04 16:20:13 +0100

    viser håvard greier

--- numerikk2.py
+++ numerikk2.py
@@ -40,15 +40,15 @@ import scipy
 
 
 class Function:
-    def __init__(self,a,b,func,n=100):
+    def __init__(self,func,a=None,b=None,n=100):
         """
         a: float or list/array. Either start value or list/array
            containing data.
         b: end point of interval.
         n: number of intervals
         """
-        self.a = a; self.b = b; self.n = n
         self.func = func
+        self.a = a; self.b = b; self.n = n
         self.x = np.linspace(self.a,self.b,self.n+1)
         self.y = self.func(self.x)
         self.dx = (self.b-self.a)/self.n
@@ -84,12 +84,13 @@ class Function:
         if n!=None:
             self.n = n
         return self.a,self.b,self.n
-    
+
     # def derivative(self,x,order=1,h=0.001):
     #     if order==1:
     #         return (func(x+h)-func(x))/h
     #     elif order==2:
     #         return (func(x+h)-2*func(x)+func(x-h))/(h**2)
+    
     def draw(self):
         plt.plot(self.x,self.y)
         plt.show()
